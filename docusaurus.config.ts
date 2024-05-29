08496a4af8f7f4265458dc7a32e7e65e66700a57
Luã Vaz <lua@luca.global>
2024-05-29 09:47:15 -0300

    configurando docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,13 @@ const config: Config = {
   url: 'https://luca.global',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'luca', // Usually your GitHub org/user name.
-  projectName: 'LUCA', // Usually your repo name.
+  organizationName: 'luca-copilot', // Usually your GitHub org/user name.
+  projectName: 'documentation', // Usually your repo name.
+  hasTrailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
